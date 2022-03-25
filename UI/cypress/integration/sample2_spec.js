6a67a5fb5f4a6b1ffad799fe2135c2e165cb24ee
amitair <amitair@gmail.com>
2022-03-25 10:24:47 -0400

    more changes

--- UI/cypress/integration/sample2_spec.js
+++ UI/cypress/integration/sample2_spec.js
@@ -1,11 +1,9 @@
-describe("multiformis verify", function () {
-  it("does stuff", function () {
-    cy.visit("http://website.multiformis.com/");
+describe("a meh test", function () {
+  it("does meh stuff", function () {
+    cy.visit("https://meh.com/");
     // cy.get("div[class=iframes-container]")
-    cy.contains("CLOSE").click();
-    cy.contains("Sort and Tables").click();
-    cy.get('[name=tablepress-2_length]').select("25");
-    cy.contains('mail_city').click();
+    cy.get(".membership").first().dblclick();
+    // cy.get(".fa fa-fw fa-question-circle").first().click();
     // cy.get('button[class^="submit_btn"]').click();
     //   cy.get("title").should("contain", "Todoist")
   });
